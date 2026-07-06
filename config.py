67376d141c855cb3de58728e081a7b33788e2c38
Kush Parekh <kushaparekh07@gmail.com>
2026-07-06 16:02:41 +0530

    Add file type categories to config.py

--- config.py
+++ config.py
@@ -1,1 +1,42 @@
 
+FILE_TYPES = {
+
+    "Images": [
+        ".png", ".jpg", ".jpeg", ".gif",
+        ".bmp", ".webp", ".svg"
+    ],
+
+    "Videos": [
+        ".mp4", ".mkv", ".mov",
+        ".avi", ".wmv"
+    ],
+
+    "Music": [
+        ".mp3", ".wav",
+        ".aac", ".flac"
+    ],
+
+    "Documents": [
+        ".pdf", ".docx",
+        ".doc", ".txt",
+        ".pptx", ".xlsx"
+    ],
+
+    "Archives": [
+        ".zip", ".rar",
+        ".7z", ".tar",
+        ".gz"
+    ],
+
+    "Programs": [
+        ".exe", ".msi",
+        ".apk"
+    ],
+
+    "Code": [
+        ".py", ".cpp", ".c",
+        ".java", ".js",
+        ".html", ".css",
+        ".json", ".xml"
+    ]
+}
